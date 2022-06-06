45e1f8e2ae6d7437953a2fbd27fdd0d2877ed560
cburgyan <cburgyan@gmail.com>
2022-06-05 22:12:38 -0500

    create list of songs

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import requests
 import bs4
 
 
-# date = input("What day do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-date = "2010-05-22"
+date = input("What day do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+# date = "2010-05-22"
 url = f"https://www.billboard.com/charts/hot-100/{date}/"
 
 html_billboard = requests.get(url).text
@@ -12,12 +12,12 @@ soup = bs4.BeautifulSoup(html_billboard, "html.parser")
 
 song_selector = "div ul li ul li h3"
 
-print(soup.select_one(selector=song_selector))
+print(soup.select_one(selector=song_selector).get_text().strip())
 
-print(soup.find(name="h3", id="title-of-a-story").get_text().strip())
+# print(soup.find(name="h3", id="title-of-a-story").get_text().strip())
 
 
-list_of_song_titles = [tag.get_text().strip() for tag in soup.find_all(name="h3", id="title-of-a-story")]
+list_of_song_titles = [tag.get_text().strip() for tag in soup.select(selector=song_selector)]
 
 print(list_of_song_titles)
 
