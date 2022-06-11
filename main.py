8cdd1755a86a09f981d46fe29c5e36fbce56026b
cburgyan <cburgyan@gmail.com>
2022-06-11 18:05:37 -0500

    get URI list and create playlist

--- main.py
+++ main.py
@@ -35,11 +35,32 @@ spoti = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(client_id=os.en
 
 print("*****************")
 print(spoti)
-user_id = spoti.current_user()
+user_id = spoti.current_user()["id"]
+print(user_id)
 # pprinter = pprint.PrettyPrinter()
 # pprinter.pprint(spoti)
-# print(spoti.)
-# playlist = spoti.user_playlist_create(user=os.environ.get("SPOTIPY_CLIENT_ID"), name="ToppiesOf2021", public=False, collaborative=False, description="2021 playlist")
-# print(playlist)
+# print(date[:4])
+song_uri_list = []
+# printed_one = False
+for song in list_of_song_titles:
+    parameters = f"track:{song} year:{date[:4]}"
+    search_result = spoti.search(q=parameters, type="track")
+    # if not printed_one:
+    #     pprint.pprint(search_result)
+    #     printed_one = True
+    try:
+        song_uri = search_result["tracks"]["items"][0]["uri"]
+        song_uri_list.append(song_uri)
+    except IndexError:
+        print(f'"{song}" is not in Spotify inventory.')
+    else:
+        print(song)
 
-# pprint.pprint(spoti)
+playlist = spoti.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False, collaborative=False, description="2021 playlist")
+# print(type(playlist))
+# print(playlist)
+# print(playlist["id"])
+# print(playlist["uri"])
+response = spoti.playlist_add_items(playlist_id=playlist["id"], items=song_uri_list)
+print(type(response))
+pprint.pprint(response)
